187a03730881b528da9f1b5d33abef4fe30491d4
HammontreeWebDev <hammontreewebdev@gmail.com>
2022-11-28 18:24:57 -0500

    created thoughtRoutes.. need to add controller

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
+
+// route to get all thoughts and route to create a thought
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// route to get single thought, update a thought, and delete a thought
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// route to create a new reaction and delete an existing one
+router.route(':thoughtId/reactions')
+.post(createReaction)
+.delete(deleteReaction);
