2af63f2cfa223bb5cb9995fa2ad19a97e54737c7
HammontreeWebDev <hammontreewebdev@gmail.com>
2022-11-28 16:09:33 -0500

    copied folder structure from mini project.. will refactor... added user model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, Types } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // regex validation for email format
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thoughts',
+      }
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'friends'
+      }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// virtual that retrieves length of the user's friends on query
+userSchema
+.virtual('friendCount')
+.get(function () {
+  return `You have ${this.friends.length} friends!`
+})
+
+const User = model('user', userSchema);
+module.exports = User;
