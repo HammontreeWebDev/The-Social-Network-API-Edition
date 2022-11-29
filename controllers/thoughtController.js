40dc4663b2778cc1f328c8b9ad0e0191be96d3c1
HammontreeWebDev <hammontreewebdev@gmail.com>
2022-11-28 20:24:59 -0500

    thoughtController finished

---
+++ controllers/thoughtController.js
@@ -0,0 +1,57 @@
+const Thought = require('../models/Thought');
+
+module.exports = {
+    getAllThoughts(req, res) {
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+    getSingleThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((dbThoughtData) => res.json(dbThoughtData))
+            .catch((err) => res.status(500).json(err));
+    },
+    updateThought(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: 'No thought with that ID' })
+                    : res.json(thought)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    createReaction(req, res) {
+        Thought.create({
+            req: req.body,
+            where: { _id: req.params.thoughtId }
+        })
+            .then((dbReactionData) => res.json(dbReactionData))
+            .catch((err) => res.status(500).json(err));
+    },
+    deleteReaction(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((dbReactionData) => res.json(dbReactionData))
+            .catch((err) => res.status(500).json(err));
+    }
+}
