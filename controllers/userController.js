69bd87eb09b51338dc039011c25a6721d91fb76e
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-11-30 15:25:00 -0500

    prev fix actually didn't do anything.. now we have actually fixed the routing issue in the controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,80 +2,84 @@ const User = require('../models/User');
 const Thought = require('../models/Thought');
 
 module.exports = {
-    getAllUsers(req, res) {
-        User.find()
-        .then((users) => res.json(users))
-        .catch((err) => res.status(500).json(err));
-    },
-    getSingleUser(req, res) {
-        User.findOne({ _id: req.params.userId })
-          .select('-__v')
-          .then((user) =>
-            !user
-              ? res.status(404).json({ message: 'No user with that ID' })
-              : res.json(user)
-          )
-          .catch((err) => res.status(500).json(err));
-      },
-      createUser(req, res) {
-        User.create(req.body)
-          .then((dbUserData) => res.json(dbUserData))
-          .catch((err) => res.status(500).json(err));
-      },
-      updateUser(req, res) {
-        User.findOneAndUpdate(
-          {_id: req.params.userId},
-          {$addToSet: {username: req.body.username}},
-          {$addToSet: {email: req.body.email}}
-          )
-        .select('-__v')
-        .then((user) => 
+  getAllUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select('-__v')
+      .then((user) =>
         !user
-        ? res.status(404).json({ message: 'No user with that ID'})
-        : res.json({ message: 'User successfully updated'}, user)
-        )
-        .catch((err) => res.status(500).json(err));
-      },
-      deleteUser(req, res) {
-        User.findOneAndDelete({_id: req.params.userId})
-        .select('-__v')
-        .then((deletedUser) => 
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      {
+          username: req.body.username,
+          email: req.body.email
+      })
+      .select('-__v')
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.status(200).json ({message: 'User successfully updated'})
+      )
+      .catch((err) => {
+        console.error(new Error);
+        res.status(500).json(err);
+      });
+  },
+  deleteUser(req, res) {
+    User.findOneAndDelete({ _id: req.params.userId })
+      .select('-__v')
+      .then((deletedUser) =>
         // tap into the Thought model and delete all thoughts that belong to the user to free up db space using the deleteMany method in conjuction with the mongoose operator $in
         !deletedUser
-        ? res.status(404).json({ message: 'No user with that ID'})
-        : Thought.deleteMany({ _id: { $in: deletedUser.thoughts } })
-        )
-        .then((deletedUser) => 
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : Thought.deleteMany({ _id: { $in: deletedUser.thoughts } })
+      )
+      .then((deletedUser) =>
         !deletedUser
-        ? res.status(404).json({ message: 'No user with that ID'})
-        : res.json({ message: 'User successfully deleted'})
-        )
-        .catch((err) => res.status(500).json(err));
-      },
-      deleteFriend(req, res) {
-        User.findOneAndUpdate(
-          { _id: req.params.userId },
-          {$pull: {friends: req.params.friendsId}},
-        )
-        .select('-__v')
-        .then((user)=> 
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({ message: 'User successfully deleted' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendsId } },
+    )
+      .select('-__v')
+      .then((user) =>
         !user
-        ? res.status(404).json({ message: 'No user with that ID'})
-        : res.json({ message: 'Friend successfully deleted'})
-        )
-        .catch((err) => res.status(500).json(err));
-      },
-      addFriend(req, res) {
-        User.findOneAndUpdate(
-          { _id: req.params.userId },
-          {$addToSet: {friends: req.params.friendsId}},
-          )
-        .select('-__v')
-        .then((user)=> 
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({ message: 'Friend successfully deleted' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.params.friendsId } },
+    )
+      .select('-__v')
+      .then((user) =>
         !user
-        ? res.status(404).json({ message: 'No user with that ID'})
-        : res.json({ message: 'Friend successfully added'})
-        )
-        .catch((err) => res.status(500).json(err));
-      },
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({ message: 'Friend successfully added' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
 }
